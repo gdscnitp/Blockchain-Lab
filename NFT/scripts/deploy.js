3e2a58cc6d9521f306234dfa82da8db1f2ce99d7
Divyansh <srivas.divyansh22@gmail.com>
2023-10-05 22:14:28 +0530

    Files of basic NFT minting and deploying files

---
+++ NFT/scripts/deploy.js
@@ -0,0 +1,14 @@
+async function main(){
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+  const myNFT = await MyNFT.deploy();
+  console.log("Contract deployed to address: ", myNFT.address);
+}
+
+main()
+.then(() => process.exit(0))
+.catch((error)=>{
+  console.error(error);
+  process.exit(1);
+})
+
+//0x5BD3D0c828a05451B0C2306737324bD394BA5174
