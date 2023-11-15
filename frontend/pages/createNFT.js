6ee5665f298525af8575b6b0acea89d5be5753af
ptl-harsh <hvpatel.me@gmail.com>
2023-11-15 11:46:08 +0530

    added the AI image generation and IPFS Upload feature

--- frontend/pages/createNFT.js
+++ frontend/pages/createNFT.js
@@ -1,57 +1,128 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from "react";
+import { NFTStorage, File } from "nft.storage";
+import { Buffer } from "buffer";
+import axios from "axios";
 
-function CreateNFT() {
+function createNFT() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+
   // State variables to manage form input
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
   const [image, setImage] = useState(null);
+  const [url, setURL] = useState(null);
+
 
   // Function to handle form submission
-  const handleCreateNFT = async (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
 
-    // Perform the NFT creation logic here, including sending data to your backend, blockchain interaction, and IPFS upload.
+    // Call AI API to generate a image based on description
+    const imageData = createImage();
 
-    // Reset the form after NFT creation
-    setTitle('');
-    setDescription('');
-    setImage(null);
+    //Upload image to IPFS (NFT.Storage)
+    const url = await uploadImage(imageData);
+
+    console.log("url", url);
   };
+
+  const createImage = async () => {
+    console.log("Generating image...");
+
+    const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0`;
+
+    const response = await axios({
+      url: URL,
+      method: "POST",
+      headers: {
+        Authorization: "Bearer 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",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify({
+        inputs: description,
+        options: { wait_for_model: true },
+      }),
+      responseType: "arraybuffer",
+    });
+
+    const type = response.headers["content-type"];
+    const data = response.data;
+
+    const base64data = Buffer.from(data).toString("base64");
+    const img = `data:${type};base64,` + base64data; // <-- This is so we can render it on the page
+    setImage(img);
+
+    return data;
+  };
+
+  const uploadImage = async (imageData) => {
+    console.log("Uploading Image...");
+
+    // Create instance to NFT.Storage
+    const nftstorage = new NFTStorage({
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEVFYzJiMDFhYjExZDcyOEVCODM5NjJhOEY0QzlkRkNBM2QyMEY0ZjUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY5ODEyNTU3NzM5MiwibmFtZSI6ImZvciBBSSBwcm8ifQ.c5c-3daVx1tyQ9eLnaTmMod7bhkSbokhOtFFHlVf-Iw",
+    });
+
+    // Send request to store image
+
+    const { ipnft } = await nftstorage.store({
+      image: new File([imageData], "image.jpeg", { type: "image/jpeg" }),
+      name: name,
+      description: description,
+    });
+
+    //save the URL
+    const url = `https://ipfs.io/ipfs/${ipnft}/metadata.json`;
+    setURL(url);
+
+    return url;
+  };
+
+  // Perform the NFT creation logic here, including sending data to your backend, blockchain interaction, and IPFS upload.
+
 
   return (
     <div>
       <h1>Create NFT</h1>
-      <form onSubmit={handleCreateNFT}>
-        <div>
-          <label>Title:</label>
+      <div className="form">
+        <form onSubmit={submitHandler}>
+          <label>Title:</label> {<br></br>}
           <input
             type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Description:</label>
-          <textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Upload Image:</label>
+            placeholder="Create a name..."
+            onChangeCapture={(e) => {
+              setName(e.target.value);
+            }}
+          />{" "}
+          {<br></br>}
+          <label>Description:</label> {<br></br>}
           <input
-            type="file"
-            accept="image/*"
-            onChange={(e) => setImage(e.target.files[0])}
-            required
-          />
+            type="text"
+            placeholder="Create a description..."
+            onChangeCapture={(e) => {
+              setDescription(e.target.value);
+            }}
+          ></input>{" "}
+          {<br></br>} {<br></br>}
+          <input type="submit" value="Create & Mint" />
+        </form>{" "}
+        {<br></br>}
+        
+        <div className="image">
+          <img src={image} alt="AI generated image" />
         </div>
-        <button type="submit">Create NFT</button>
-      </form>
+      </div>
+      <p>
+        View&nbsp;{" "}
+        <a href={url} target="_blank" rel="noreferrer">
+          Metadata
+        </a>
+      </p>
     </div>
   );
 }
 
-export default CreateNFT;
+export default createNFT;
